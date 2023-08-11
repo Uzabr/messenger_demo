df43a4f09834146b93f3357e57e28736293349dd
Abror <khaitboboev94@mail.ru>
2023-08-11 21:15:43 +0300

    add annotation

--- src/main/java/com/example/entity/Comment.java
+++ src/main/java/com/example/entity/Comment.java
@@ -1,17 +1,32 @@
 package com.example.entity;
 
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+
+    @Column(nullable = false)
     private String userName;
+
+    @Column(nullable = false)
     private Long userId;
+
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,14 +1,13 @@
 package com.example.entity;
 
 import com.example.entity.roles.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Data
 @Entity
@@ -35,11 +34,23 @@ public class User {
     @Column(length = 3000)
     private String password;
 
-    
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn("user_id"))
     private Set<ERole> role = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
     private List<Post> postList = new ArrayList<>();
 
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
     private LocalDateTime createDate;
+
+    @Transient
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+
+    }
 
     @PrePersist
     protected void onCreate() {
